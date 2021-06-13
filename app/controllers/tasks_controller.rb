450393dcacc15e2249efc4e748523bcf5ba95116
akitayuri <nmnl_y.bgux@icloud.com>
2021-06-13 17:26:39 +0900

    ceate index action and view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,9 +7,20 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    Task.create(task_params)
+    redirect_to new_task_path
+  end
+
   def edit
   end
 
   def show
   end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+
 end
