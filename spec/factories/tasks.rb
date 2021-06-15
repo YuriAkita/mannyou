2cf6b41a5ae35069b68497a79285d760ac667efc
akitayuri <nmnl_y.bgux@icloud.com>
2021-06-15 21:48:45 +0900

    add rspec test code for deadline

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,10 +3,18 @@ FactoryBot.define do
     # 下記の内容は実際に作成するカラム名に合わせて変更してください
     title { 'Factoryで作ったデフォルトのタイトル１' }
     content { 'Factoryで作ったデフォルトのコンテント１' }
+    task_deadline { '002021-08-01' }
+  end
+
+  factory :task_deadline do
+    title { 'Factoryで作った終了期限が一番上にくるはずのタイトル2' }
+    content { 'Factoryで作った終了期限が一番上にくるはずのコンテント2' }
+    task_deadline { '002021-10-01' }
   end
 
   factory :second_task, class: Task do
     title { 'Factoryで作ったデフォルトのタイトル２' }
     content { 'Factoryで作ったデフォルトのコンテント２' }
+    task_deadline { '002021-12-30' }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,28 +2,32 @@ require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
   before do
     @task = FactoryBot.create(:task, title: '該当タスクの内容が表示される')
-    task1 = FactoryBot.create(:task, title: 'task1')
-    task2 = FactoryBot.create(:task, title: 'task2')
-    task3 = FactoryBot.create(:task, title: 'task3')
+    task1 = FactoryBot.create(:task, title: 'task1', task_deadline: '002021-11-01')
+    task2 = FactoryBot.create(:task, title: 'task2', task_deadline: '002021-12-01')
+    task3 = FactoryBot.create(:task, title: 'task3', task_deadline: '002022-01-01')
   end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
-       # 1. new_task_pathに遷移する（新規作成ページに遷移する）
        visit new_task_path
-       # 2. 新規登録内容を入力する
-       #「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄にタスクのタイトルと内容をそれぞれ入力する
        # binding.irb
        fill_in 'task[title]', with: 'test_title'
        fill_in 'task[content]', with: 'test_content'
-       # 3. 「登録する」というvalue（表記文字）のあるボタンをクリックする
-       # ここに「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
+       fill_in 'task[task_deadline]', with: '002021-12-24'
+       # binding.irb
        click_button '投稿する'
-       # 4. clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
-       # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
        expect(page).to have_content 'test_title'
        expect(page).to have_content 'test_content'
-       # ここにタスク詳細ページに、テストコードで作成したデータがタスク詳細画面にhave_contentされているか（含まれているか）を確認（期待）するコードを書く
+       expect(page).to have_content '2021-12-24'
+      end
+    end
+
+    context '終了期限でソートするをクリックした場合' do
+      it '終了期限が一番長いものが一番上に表示される' do
+        visit tasks_path
+        task_list = all('.task_row')
+        click_link '終了期限でソートする'
+        expect(task_list[0]).to have_content 'task3'
       end
     end
   end
