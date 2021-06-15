a4843ad9d1057796d915d52ddd693758995216f3
akitayuri <nmnl_y.bgux@icloud.com>
2021-06-16 00:06:40 +0900

    fix tasks model

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,4 @@ class Task < ApplicationRecord
   enum status: { 未着手: 0, 着手中: 1, 完了: 2 }
   scope :title_search, -> (title) { where("title LIKE ?", "%#{title}%") }
   scope :status_search, -> (status) { where(status: status) }
-  # scope :title_search -> (title) { where('title LIKE ?', "%#{title}%") }
-  # scope :status_search -> (status) { where(status: status) }
 end
