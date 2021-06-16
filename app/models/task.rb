84d7150fdbc7e4b82d620311fe7fc661f975cefb
akitayuri <nmnl_y.bgux@icloud.com>
2021-06-16 15:00:24 +0900

    add column priprity to tasks table

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,5 @@ class Task < ApplicationRecord
   enum status: { 未着手: 0, 着手中: 1, 完了: 2 }
   scope :title_search, -> (title) { where("title LIKE ?", "%#{title}%") }
   scope :status_search, -> (status) { where(status: status) }
+  enum priority: { 高: 0, 中: 1, 低: 2 }
 end
