759b5cad15d6c0344c2b0e5d307e5af25ebf3f13
akitayuri <nmnl_y.bgux@icloud.com>
2021-06-17 16:30:14 +0900

    add not null to Tasks task_deadline

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_055715) do
+ActiveRecord::Schema.define(version: 2021_06_17_072304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_06_16_055715) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.date "task_deadline"
+    t.date "task_deadline", null: false
     t.integer "status", default: 0, null: false
     t.integer "priority", default: 0, null: false
     t.index ["title"], name: "index_tasks_on_title"
