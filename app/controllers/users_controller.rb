7d46d413daf3eb5751f058e606d9f20337fe7304
akitayuri <nmnl_y.bgux@icloud.com>
2021-06-18 03:52:46 +0900

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+<<<<<<< Updated upstream
   skip_before_action :login_required, only: [:new, :create]
 
+=======
+>>>>>>> Stashed changes
   def new
     @user = User.new
   end
@@ -8,14 +11,20 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+<<<<<<< Updated upstream
       redirect_to user_path(@user.id)
+=======
+>>>>>>> Stashed changes
     else
       render :new
     end
   end
 
   def show
+<<<<<<< Updated upstream
     @user = User.find(params[:id])
+=======
+>>>>>>> Stashed changes
   end
 
   private
