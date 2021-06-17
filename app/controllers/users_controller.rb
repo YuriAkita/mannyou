1bcca5cd03ca5240a0f41bf77d5adb7f5efabbcd
akitayuri <nmnl_y.bgux@icloud.com>
2021-06-18 04:58:06 +0900

    add users create action redirect link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      redirect_to tasks_path, notice: 'ログインしました！'
     else
       render :new
     end
