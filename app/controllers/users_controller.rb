dc8bb1c2c76e8e8bc6c21f7e283dc5119fe84457
akitayuri <nmnl_y.bgux@icloud.com>
2021-06-18 02:47:00 +0900

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :login_required, only: [:new, :create]
-<<<<<<< Updated upstream
 
-=======
-  
->>>>>>> Stashed changes
   def new
     @user = User.new
   end
