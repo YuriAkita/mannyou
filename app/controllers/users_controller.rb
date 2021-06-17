69426667dc84e1b303bba8b9cb412a4aec6616e4
akitayuri <nmnl_y.bgux@icloud.com>
2021-06-18 03:53:46 +0900

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  skip_before_action :login_required, only: [:new, :create]
-
   def new
     @user = User.new
   end
@@ -8,14 +6,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
     else
       render :new
     end
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   private
