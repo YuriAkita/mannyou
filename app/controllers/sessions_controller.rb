dff9b7a8043f4f1844aeed88159911f19aba0cd1
akitayuri <nmnl_y.bgux@icloud.com>
2021-06-18 03:00:38 +0900

    fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_action :login_required, only: [:new, :create]
-  
+
   def new
   end
 
@@ -17,5 +17,8 @@ class SessionsController < ApplicationController
 
   def destroy
     session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
   end
+
 end
